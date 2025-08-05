d8a0c1679310fa46842cb5e748c92a0dc9cd4f3b
VoinzzZ <antonynugroho467@gmail.com>
2025-08-06 00:06:07 +0700

    prisma init db mysql

--- pos-backend/server.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-const app = require('./src/app');
-
-dotenv.config();
-const PORT = process.env.PORT || 11000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-}); 
