3802d38966dda9a5581b7fec0e516b3daf76dca5
VoinzzZ <antonynugroho467@gmail.com>
2025-08-07 00:09:44 +0700

    jwt services

---
+++ server-pos-api/app/utils/jwt.js
@@ -0,0 +1,56 @@
+const jwt = require('jsonwebtoken');
+
+class JWTService {
+    // Generate Token (15minutes)
+    static generateAccessToken(payLoad) {
+        return jwt.sign(
+            payLoad,
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+                issuer: 'pos-system',
+                audience: 'pos-users'
+            }
+        );
+    }
+
+    // Generate Refresh Token (7days)
+    static generateRefreshToken(payLoad) {
+        return jwt.sign(
+            payLoad,
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+                issuer: 'pos-system',
+                audience: 'pos-users'
+            }
+        );
+    }
+
+    // Verify Token
+    static verifyToken(token) {
+        try {
+            return jwt.verify(token, process.env.JWT_SECRET, {
+                issuer: 'pos-system',
+                audience: 'pos-users'
+            });
+        } catch (error) {
+            throw new Error(`Token Verification failed: ${error.message}`);
+        }
+    }
+
+    // Decode Token (without verification)
+    static decodeToken(token) {
+        return jwt.decode(token);
+    }
+
+    // Generated Token Pair (Access + Refresh)
+    static generateTokenPair(payLoad) {
+        return {
+            accesToken: this.generateAccessToken(payLoad),
+            refreshToken: this.generateRefreshToken(payLoad)
+        };
+    }
+}
+
+module.exports = JWTService;
