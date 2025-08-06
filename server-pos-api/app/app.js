2dba30bf1aae7d2b229550ec6497613ade954d73
VoinzzZ <antonynugroho467@gmail.com>
2025-08-06 14:18:45 +0700

    error global handler

--- server-pos-api/app/app.js
+++ server-pos-api/app/app.js
@@ -6,7 +6,6 @@ const rateLimit = require('express-rate-limit');
 
 const app = express();
 
-
 app.use(helmet());
 const limiter = rateLimit({
     windowMS: 15 * 60 * 1000, // 15minutes
@@ -22,3 +21,27 @@ app.use(cors({
     credentials: true,
     optionsSuccessStatus: 200
 }));
+app.use(express.json({ limit: '10m' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieparser(process.env.COOKIES_SECRET));
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: 'POS API IS RUNNING',
+        timestamp: new Date().toISOString()
+    });
+});
+app.use('*', (req, res) => {
+    res.json({
+        success: false,
+        message: `Route ${req.originalUrl} not found`
+    });
+});
+app.use((error, req, res, next) => {
+    console.log('Error: ', error.message);
+    res.status(400).json({
+        success: false,
+        message: error.message || 'Internal Server Error',
+        ...(process.env.NODE_ENV === 'development' && { stack: error.stack })
+    });
+});
