8fe2f9facd3e59e601cbf993cf71775260cf65d3
VoinzzZ <antonynugroho467@gmail.com>
2025-08-11 10:26:59 +0700

    mail config fix

---
+++ server-pos-api/app/config/mail.config.js
@@ -0,0 +1,20 @@
+const nodemailer = require('nodemailer');
+
+class MailConfig {
+    static createTransporter() {
+        return nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: Number(process.env.SMTP_PORT),
+            secure: process.env.SMTP_SECURE === 'true',
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
+    }
+}
+
+module.exports = MailConfig;
