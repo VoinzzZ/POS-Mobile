67fb92dc8e4b8c9bdf7b025295362c3fd73d9b22
VoinzzZ <antonynugroho467@gmail.com>
2025-08-13 13:44:53 +0700

    regist login page route

--- frontend-pos/src/App.jsx
+++ frontend-pos/src/App.jsx
@@ -1,3 +1,14 @@
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import LoginPage from "./views/LoginPage"
+import RegistrationPage from "./views/RegistratiomPage"
 
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<RegistrationPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Routes>
+    </Router>
+  )
 }
