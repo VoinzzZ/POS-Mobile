5c62d74a2fed8590afe7679d8d912cfc86d4f819
VoinzzZ <antonynugroho467@gmail.com>
2025-08-21 10:13:24 +0700

    fix login screen

--- client-mobile/app/(auth)/_layout.tsx
+++ client-mobile/app/(auth)/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function AuthLayout() {
-    return <Stack screenOptions={{ headerShown: false }} />;
+    return (
+        <>
+        <StatusBar barStyle={"dark-content"} backgroundColor="transparent" translucent/>
+        <Stack screenOptions={{ headerShown: false }} />;
+        </>
+    )
 }
