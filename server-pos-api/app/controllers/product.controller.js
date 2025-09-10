bda8f5d292541e0e38b50df5ba3789ab2114b1f4
VoinzzZ <antonynugroho467@gmail.com>
2025-09-10 14:50:10 +0700

    add: add product

--- server-pos-api/app/controllers/product.controller.js
+++ server-pos-api/app/controllers/product.controller.js
@@ -12,6 +12,21 @@ class ProductController extends BaseController {
         const products = await this.prisma.product.findMany();
         return this.sendSuccess(res, { message: 'Products fetched successfully', data: products });
     });
+
+    // POST add new product
+    addProduct = this.asyncHandler(async (req, res) => {
+        const { name, brand, type, price, stock, imageUrl } = req.body;
+        this.validateRequiredFields(req.body, ['name', 'price', 'stock']);
+
+        const newProduct = await this.prisma.$transaction(async (tx) => {
+            const product = await tx.product.create({
+                data: { name, brand, type, price, stock, imageUrl }
+            });
+            return product;
+        });
+
+        return this.sendSuccess(res, { statusCode: 201, message: 'Product created successfully', data: newProduct });
+    });
     
 }
 
