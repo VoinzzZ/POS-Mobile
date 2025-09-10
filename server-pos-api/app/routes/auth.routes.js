6f5cc6fd555e8d622db81d9e525cfac6507af8c8
VoinzzZ <antonynugroho467@gmail.com>
2025-09-10 11:19:25 +0700

    add: auth limitter and soeed limiter

--- server-pos-api/app/routes/auth.routes.js
+++ server-pos-api/app/routes/auth.routes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const AuthController = require('../controllers/auth.controller');
 const AuthMiddleware = require('../middlewares/auth.middleware');
+const { authLimiter, speedLimiter } = require('../middlewares/rateLimiter')
 
 const router = express.Router();
 
 // instance
 const authController = new AuthController();
-const authMiddleware = new AuthMiddleware();
 
 // Public routes (multi-step register)
-router.post('/register', authController.register);             
-router.post('/send-email-code', authController.sendEmailOTP); 
-router.post('/verify-email-code', authController.verifyEmailOTP); 
+router.post('/register', authLimiter, speedLimiter, authController.register);             
+router.post('/send-email-code', authLimiter, speedLimiter, authController.sendEmailOTP); 
+router.post('/verify-email-code', authLimiter, speedLimiter,    authController.verifyEmailOTP); 
 router.post('/set-password', authController.setPassword);
 
 // Auth routes
-router.post('/login', authController.login);
+router.post('/login', authLimiter, speedLimiter, authController.login);
 
 // Protected routes
 router.post('/logout', AuthMiddleware.verifyToken, authController.logout);
