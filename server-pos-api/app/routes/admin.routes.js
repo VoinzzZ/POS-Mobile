5727b5bbf8c25207bd1227c5425415ac70b3e779
VoinzzZ <antonynugroho467@gmail.com>
2025-09-11 14:21:52 +0700

    add: listpins, deletepins, show pinstat router

--- server-pos-api/app/routes/admin.routes.js
+++ server-pos-api/app/routes/admin.routes.js
@@ -3,10 +3,11 @@ const AuthMiddleware = require('../middlewares/auth.middleware');
 const AdminController = require('../controllers/admin.controller');
 
 const router = express.Router();
-
-// instance
 const adminController = new AdminController();
 
-router.post('/generate-pin', AuthMiddleware.verifyToken, AuthMiddleware.requireAdmin,  adminController.generatePin)
+router.post('/generate-pin', AuthMiddleware.verifyToken, AuthMiddleware.requireAdmin, adminController.generatePin);
+router.get('/pins', AuthMiddleware.verifyToken, AuthMiddleware.requireAdmin, adminController.listPins);
+router.delete('/pins/:pinId', AuthMiddleware.verifyToken, AuthMiddleware.requireAdmin, adminController.revokePin);
+router.get('/pins/stats', AuthMiddleware.verifyToken, AuthMiddleware.requireAdmin, adminController.getPinStats)
 
 module.exports = router;
