fe8d2c0c994395dd420b4fc5a35b9f5f23df39a4
VoinzzZ <antonynugroho467@gmail.com>
2025-09-11 11:07:10 +0700

    delete logout

--- server-pos-api/app/routes/auth.routes.js
+++ server-pos-api/app/routes/auth.routes.js
@@ -18,7 +18,6 @@ router.post('/set-password', authController.setPassword);
 router.post('/login', authLimiter, speedLimiter, authController.login);
 
 // Protected routes
-router.post('/logout', AuthMiddleware.verifyToken, authController.logout);
 router.post('/refresh-token', AuthMiddleware.verifyRefreshToken, authController.refreshToken);
 router.get('/profile', AuthMiddleware.verifyToken, authController.getProfile);
 router.post('/change-password', AuthMiddleware.verifyToken, authController.changePassword);
