9103cb33435c3499a5ed08888815a1bbcfa4964c
VoinzzZ <antonynugroho467@gmail.com>
2025-09-15 16:28:53 +0700

    add: brand categoruy routes

--- server-pos-api/app/routes/index.routes.js
+++ server-pos-api/app/routes/index.routes.js
@@ -3,11 +3,15 @@ const express = require('express');
 const authRoutes = require('./auth.routes');
 const adminRoutes = require('./admin.routes');
 const productRoutes = require('./product.route');
+const brandRoutes = require('./brand.routes');
+const categoryRoutes = require('./category.routes');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/admin', adminRoutes);
 router.use('/product', productRoutes);
+router.use('./brand', brandRoutes);
+router.use('./category', categoryRoutes);
 
 module.exports = router;
