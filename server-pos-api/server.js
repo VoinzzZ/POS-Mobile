ec5e6159eb386e9f744e3a0c80530270a31d9ed6
Antony Kurniawan <antonynugroho467@gmail.com>
2025-10-06 13:55:09 +0700

    add schedule

--- server-pos-api/server.js
+++ server-pos-api/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const app = require('./app/app');
+const { startScheduler } = require('./app/services/scheduler.service');
 
 const PORT = process.env.PORT || 8888;
 
@@ -7,4 +8,7 @@ app.listen(PORT, '0.0.0.0', () => {
     console.log(`API is running on PORT: ${PORT}`);
     console.log(`Local: http://localhost:${PORT}`);
     console.log(`Network: http://192.168.10.33:${PORT}`);
+    
+    // Start auto-lock scheduler
+    startScheduler();
 })
